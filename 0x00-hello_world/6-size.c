4111946027372849635421bf80d36f0cadcb2fc7
dennismainachege <dennismainachege@gmail.com>
2023-02-16 19:23:10 -0800

    c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Aprogram to show value of comp types
+ * return: 0 (success)
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+printf("size of char:%lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of int:%lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of long int:%lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size oflong  long int:%lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of float:%lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
