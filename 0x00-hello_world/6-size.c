034e281ba4445c1ae290b03f2250ee46419922ea
dennismainachege <dennismainachege@gmail.com>
2023-02-17 04:06:04 -0800

    6-sixe.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - A program to shows the values of different computer types 
+ * main - Entry point
  *
- * return: 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
-{
-char a;
-int b;
-long int c;
-long long int d;
-float f;
-printf("size of char:%lu byte(s)\n", (unsigned long)sizeof(char));
-printf("size of int:%lu byte(s)\n", (unsigned long)sizeof(int));
-printf("size of long int:%lu byte(s)\n", (unsigned long)sizeof(long int));
-printf("size oflong  long int:%lu byte(s)\n", (unsigned long)sizeof(long long int));
-printf("size of float:%lu byte(s)\n", (unsigned long)sizeof(float));
+{printf(“Size of a char : % lu byte(s)\n”, sizeof(char));
+printf(“Size of an int : % lu byte(s)\n”, sizeof(int));
+printf(“Size of a long int : % lu byte(s)\n”, sizeof(long int));
+printf(“Size of a long long int : % lu byte(s)\n”, sizeof(long long int));
+printf(“Size of a float : % lu byte(s)\n”, sizeof(float));
 return (0);
 }
+
