e4788cefeafef06e24f6b1047770e54d0a3707e9
dennismainachege <dennismainachege@gmail.com>
2023-02-16 18:41:24 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+ * main - A function that prints a line using printf function
+ * Return : 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
