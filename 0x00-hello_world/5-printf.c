51cff411564fd38cca94be3b53732245bd8cad75
dennismainachege <dennismainachege@gmail.com>
2023-02-16 18:57:58 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - A function that prints a line using printf function
- * Return : 0 (success)
+ * Return: 0 (success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
