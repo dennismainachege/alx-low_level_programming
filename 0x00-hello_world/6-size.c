679c50068b362bcfd2bf5c581078d1a0ec478bd6
dennismainachege <dennismainachege@gmail.com>
2023-02-20 02:24:20 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-printf(“Size of a char : %i byte(s)\n”, sizeof(char));
+printf(“Size of a char : % i i byte(s)\n”, sizeof(char));
 printf(“Size of an int : % i byte(s)\n”, sizeof(int));
-printf(“Size of a long int :% i byte(s)\n”, sizeof(long int));
+printf(“Size of a long int : % i byte(s)\n”, sizeof(long int));
 printf(“Size of a long long int : % i byte(s)\n”, sizeof(long long int));
 printf(“Size of a float : % i byte(s)\n”, sizeof(float));
 return (0);
