6523969c1124c981a3244b46c38046ca303cd764
dennismainachege <dennismainachege@gmail.com>
2023-02-20 12:48:50 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main -Entry
- * Return: 1
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
  */
 int main(void)
 {
-	write(1, "and that piece of art is usefu\"
-- Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+	return (1);
 }
