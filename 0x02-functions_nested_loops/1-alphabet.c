de94ed80581b2fa7852662f7aa1061e2eebb3e7f
dennismainachege <dennismainachege@gmail.com>
2023-02-21 12:51:58 -0800

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/*8
+ * main - prints alphabets in lowercase.
+ *
+ * Return:Always 0.
+ *
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z' ; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	return (0) ;
+}
