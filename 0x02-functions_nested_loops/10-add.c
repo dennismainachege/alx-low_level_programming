92a45da90ef2c3417887f204ffeb6e274d4c85a0
dennismainachege <dennismainachege@gmail.com>
2023-02-22 14:51:14 -0800

    100

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two integers and return the results
+ * @a: int to be added to b
+ * @b: int to added to a
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
