3b5171523d3ae2e4d8be8fa744d7658e3d8caa5f
dennismainachege <dennismainachege@gmail.com>
2023-02-27 02:15:48 -0800

    swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-/** swap-int - swaps the values of two integers.
+/**
+ * swap_int - swaps the values of two integers.
  * @a: The first integer swapped.
  * @b: The second integer swapped.
+ *
  * Return: Nothing
  */
 void swap_int(int *a, int *b)
-/* function that swaps values of integers. */
+/* function that swaps values of integers.*/
 {
 int temp = *a;
 *a = *b;
