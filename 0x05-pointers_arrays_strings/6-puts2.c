d088874fd80d3459c4dcb4a64739e9c1d72d0e92
dennismainachege <dennismainachege@gmail.com>
2023-02-27 18:54:41 -0800

    tr

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * puts - prints every other character of a string
+ * puts2 - prints every other character of a string
  * @str: The string to be treated
  * Return; Void
  */
