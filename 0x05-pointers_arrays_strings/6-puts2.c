1296efbd05c2f4f372cf16dca43bb0a23a64e37d
dennismainachege <dennismainachege@gmail.com>
2023-02-28 05:24:30 +0300

    a

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts - prints every other character of a string
+ * @str: The string to be treated
+ * Return; Void
+ */
+void puts(char *str)
+{
+	int i;
+	int j = 0;
+	while (str[j} != '\0')
+{
+	j++;
+}
+        for (i = 0; i<j ; i+=2)
+{
+	putchar (str[i]);
+}
+putchar('\n');
+}
