87417bae40cfdce55621c9a087ea3ed76bef5cea
dennismainachege <dennismainachege@gmail.com>
2023-03-02 04:00:40 -0800

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -23,6 +23,6 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[index++])
 		dest_len++;
 	for (index = 0; src[index] && index < n; index++)
-		dast[dest_len++] = src[index];
+		dest[dest_len++] = src[index];
 	return (dest);
 }
