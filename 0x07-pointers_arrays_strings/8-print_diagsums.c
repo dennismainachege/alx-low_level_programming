09d4e39caf108d5e241ed3c904cd4356f7536e88
dennismainachege <dennismainachege@gmail.com>
2023-03-06 13:35:48 -0800

    daigsum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -4,12 +4,12 @@
 /**
  * print_diagsums - Entry point
  * @a: input
- * @sie: input
+ * @size: input
  * Return : 0
  */
 void print_diagsums(int *a, int size)
 {
-	int i, n , sum1 = 0, sum2 = 0;
+	int i, n, sum1 = 0, sum2 = 0;
 
 	for (i = 0; i <= (size * size); i = i + size + 1)
 		sum1 = sum1 + a[i];
