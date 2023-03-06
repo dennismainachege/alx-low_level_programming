7c815e1729ea5c205bfe78c97815e72468c77e9a
dennismainachege <dennismainachege@gmail.com>
2023-03-06 13:30:11 -0800

    daigsum

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, n;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (n = 0; n < 8; n++)
+		        printf(a[i][n]);
+		printf('\n');
+	}
+}
