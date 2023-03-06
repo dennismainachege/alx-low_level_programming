8b2122005f1680ebc64715c1597a41f3af7f715b
dennismainachege <dennismainachege@gmail.com>
2023-03-06 11:37:01 -0800

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,9 +11,9 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i, n;
 
-	for(i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (n = 0; accept[n] !='\0'; n++)
+		for (n = 0; accept[n] != '\0'; n++)
 		{
 			if (s[i] == accept[n])
 				return (s + i);
