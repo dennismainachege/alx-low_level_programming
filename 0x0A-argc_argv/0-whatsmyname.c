242d07949c5f8443e39d850b58572df195754588
dennismainachege <dennismainachege@gmail.com>
2023-03-11 14:08:26 -0800

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+printf("%s\n", *argv);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+in this folder I get to do argc and argv commands of c
+#!/bin/bash
