b25a4f3433dbadf9ae19881e919b3a3377dcaa99
dennismainachege <dennismainachege@gmail.com>
2023-03-16 07:05:24 -0700

    0 malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Alloctes memory
+ * @b: The sie to allocate
+ *
+ * Return: Nothing.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+ THIS is a folder where i get to do more malloc free tasks
