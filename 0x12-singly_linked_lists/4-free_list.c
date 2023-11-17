5ec4e431866fc9526d8e9b2a7d53c81cea1bca2a
dennismainachege <dennismainachege@gmail.com>
2023-11-16 18:45:20 -0800

    last commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - function that frees a list_t list
+ * @head: input header pointer
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+list_t *tmp;
+
+while (head != NULL)
+{
+tmp = head;
+head = head->next;
+free(tmp->str);
+free(tmp);
+}
+}
